74fe5ca3ac6d67a81e59b6b1209be42530ca5b0b
Cirrus CI <support@cirruslabs.org>
2026-03-16 21:10:14 +0000

    si

--- tarea/api/lib/main.dart
+++ tarea/api/lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Rick & Morty Dex',
+      title: 'Rick & Morty',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
